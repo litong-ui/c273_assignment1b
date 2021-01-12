0fc3870a11e6a7a54b4324805ee34884abf8ff5c
li tong <lililililitongg@gmail.com>
2021-01-12 14:17:33 +0800

    edit dbfunctions

--- dbFunctions.php
+++ dbFunctions.php
@@ -1,9 +1,34 @@
 <?php
-$HOST = 'localhost';
+/*$HOST = 'localhost';
 $USERNAME = 'root';
 $PASSWORD = '';
 $DB = 'c273_assignment1b';
 $link = mysqli_connect($HOST,$USERNAME,$PASSWORD,$DB) or die(mysqli_connect_error());
+*/
+  $connectstr_dbhost = '';
+  $connectstr_dbname = '';
+  $connectstr_dbusername = '';
+  $connectstr_dbpassword = '';
+
+  foreach ($_SERVER as $key => $value) {
+  if (strpos($key, "MYSQLCONNSTR_localdb") !== 0) {
+  continue;
+  }
+
+  $connectstr_dbhost = preg_replace("/^.*Data Source=(.+?);.*$/", "\\1", $value);
+  $connectstr_dbname = preg_replace("/^.*Database=(.+?);.*$/", "\\1", $value);
+  $connectstr_dbusername = preg_replace("/^.*User Id=(.+?);.*$/", "\\1", $value);
+  $connectstr_dbpassword = preg_replace("/^.*Password=(.+?)$/", "\\1", $value);
+  }
+
+  $link = mysqli_connect($connectstr_dbhost, $connectstr_dbusername, $connectstr_dbpassword,$connectstr_dbname);
+
+  if (!$link) {
+  echo "Error: Unable to connect to MySQL." . PHP_EOL;
+  echo "Debugging errno: " . mysqli_connect_errno() . PHP_EOL;
+  echo "Debugging error: " . mysqli_connect_error() . PHP_EOL;
+  exit;
+  }
 ?>
 
 /*Li Tong
